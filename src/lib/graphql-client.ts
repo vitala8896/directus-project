ebc4e1005d443fc3ce10005f321e2e96e43c33eb
Vitaliy <vitala8896@gmail.com>
2025-05-26 10:00:14 +0300

    cache: 'no-store'

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
-const endpoint = 'https://directus-production-2529.up.railway.app/graphql';
-
-export const graphQLClient = new GraphQLClient(endpoint);
+export const graphQLClient = new GraphQLClient('https://directus-production-2529.up.railway.app/graphql', {
+  fetch: (input, init) => fetch(input, { ...init, cache: 'no-store' }),
+});
